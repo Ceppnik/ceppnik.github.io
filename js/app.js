3ae4a6cd645076f70eda1c1f60f2356329064fc4
Nikorda <nikorde@gmail.com>
2020-11-05 17:37:31 +0300

    add

--- js/app.js
+++ js/app.js
@@ -54,54 +54,21 @@ $(document).ready(function () {
     sectionObserver.observe(counterSection);
 
 
-    // image filter
 
-    var $wrapper = $('.portfolio__wrapper')
 
-    //Initialize isotope
-    $wrapper.isotope({
-        filter: '*',
-        layoutMode: 'masonry',
-        animationOptions: {
-            duration: 750,
-            easing: 'linear'
+
+    function getAge(dateString) {
+        var today = new Date();
+        var birthDate = new Date(dateString);
+        var age = today.getFullYear() - birthDate.getFullYear();
+        var m = today.getMonth() - birthDate.getMonth();
+        if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
         }
+        return age;
+    }
 
-    })
-
-    let links = document.querySelectorAll('.tabs a');
-    links.forEach(link => {
-        let selector = link.dataset.filter;
-        link.addEventListener('click', function (e) {
-            e.preventDefault();
-            $wrapper.isotope({
-                filter: selector,
-                layoutMode: 'masonry',
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear'
-                }
-
-            })
-
-            links.forEach(link => {
-                link.classList.remove('active');
-            });
-            e.target.classList.add('active');
-        })
-    })
-
-    // Magnify popup
-
-    $('.magnify').magnificPopup({
-        type: 'image',
-        gallery: {
-            enabled: true
-        },
-        zoom: {
-            enable: true
-        }
-    })
+    document.getElementById("years").innerHTML = getAge("1988-03-25") + " years";
 
 })
 
